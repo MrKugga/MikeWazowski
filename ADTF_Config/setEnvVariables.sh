dfbcf50eeb7a640141153fd0a23f79da5e6c4e4f
ADAS_EE5 <stefavitra@gmail.com>
2026-05-19 22:15:59 +0200

    env variables

--- ADTF_Config/setEnvVariables.sh
+++ ADTF_Config/setEnvVariables.sh
@@ -1,4 +1,4 @@
 
-export ADTF_LICENSER_PLUGIN="../adtf/bin/licenser.so"
-export ADTF_sdk_DIR="/home/adas-ee5/AudiProjects/Assignment1_ADTF_Plugin/adtf_sdk"
-export adtf_runtime_DIR="/home/adas-ee5/AudiProjects/Assignment1_ADTF_Plugin/adtf"
+export ADTF_LICENSER_PLUGIN="/home/adas-ee5/Desktop/ADTF/adtf/bin/licenser.so"
+export ADTF_sdk_DIR="/home/adas-ee5/Desktop/ADTF/adtf_sdk"
+export adtf_runtime_DIR="/home/adas-ee5/Desktop/ADTF/adtf"
