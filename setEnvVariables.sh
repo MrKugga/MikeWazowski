0611c01c26df1639e670c88cc3d981bc4ce154d7
Yannick Giovanakis <yannick.giovanakis@italdesign.it>
2026-05-26 21:20:24 +0200

    Remove setEnvVaraibles.sh from tracking — added to .gitignore

--- setEnvVariables.sh
+++
@@ -1,4 +0,0 @@
-
-export ADTF_LICENSER_PLUGIN="/home/adas-ee5/Desktop/ADTF/adtf/bin/licenser.so"
-export ADTF_sdk_DIR="/home/adas-ee5/Desktop/ADTF/adtf_sdk"
-export adtf_runtime_DIR="/home/adas-ee5/Desktop/ADTF/adtf"
